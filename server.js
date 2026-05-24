b937fc6e67c91e55b08a66d0ca6acb6ac2351736
Ovaispecial <75292449+Ovaispecial@users.noreply.github.com>
2026-05-24 22:42:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,17 @@
-require('dotenv').config()
-
 const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
 
 const app = express()
 
-app.use(cors())
-app.use(express.json())
+app.get('/', (req, res) => {
+    res.send('HOME WORKING')
+})
+
 app.get('/test', (req, res) => {
     res.send('TEST WORKING')
-})
-mongoose.connect(process.env.MONGODB_URI)
-.then(() => console.log('MongoDB Connected'))
-.catch((err) => console.log(err))
-
-const authRoutes = require('./routes/authRoutes')
-
-app.use('/api/auth', authRoutes)
-
-app.get('/', (req, res) => {
-    res.send('FIXORA Backend Running Successfully')
 })
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on ${PORT}`)
 })
