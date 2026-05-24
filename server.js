083167b3fb9a1a2e41ea46865bd299ea73cde7d1
Ovaispecial <75292449+Ovaispecial@users.noreply.github.com>
2026-05-24 20:32:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-
+app.use('/api/auth', require('./routes/authRoutes'))
 app.get('/', (req, res) => {
   res.send('FIXORA Backend Running Successfully')
 })
