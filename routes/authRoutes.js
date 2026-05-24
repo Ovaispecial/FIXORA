c260982a73cc17f575fb404afe3d8a5bf7504b66
Ovaispecial <75292449+Ovaispecial@users.noreply.github.com>
2026-05-24 23:41:02 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const bcrypt = require('bcryptjs')
-
+const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 router.get('/test', (req, res) => {
     res.json({ message: 'Auth Route Working' })
