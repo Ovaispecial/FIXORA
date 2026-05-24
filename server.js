6399a14e3c28c15a06f090b197e25bff6ccf208e
Ovaispecial <75292449+Ovaispecial@users.noreply.github.com>
2026-05-24 22:55:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+require('dotenv').config()
+
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
 const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+console.log('SERVER UPDATED')
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('MongoDB Connected'))
+.catch((err) => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('HOME WORKING')
@@ -10,8 +23,10 @@ app.get('/test', (req, res) => {
     res.send('TEST WORKING')
 })
 
+app.use('/api/auth', require('./routes/authRoutes'))
+
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
