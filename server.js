2b4e75d6f0eac1d62a0179332857fdee21ab27e2
Ovaispecial <75292449+Ovaispecial@users.noreply.github.com>
2026-05-24 21:49:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-
+app.get('/test', (req, res) => {
+    res.send('TEST WORKING')
+})
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch((err) => console.log(err))
