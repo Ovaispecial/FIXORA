d02ab362364ba08b940f4d82ba1dedfcc6bffe0d
Ovaispecial <75292449+Ovaispecial@users.noreply.github.com>
2026-05-24 23:28:51 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,24 +1,30 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-  fullname: {
-    type: String,
-    required: true
-  },
+    fullname: {
+        type: String,
+        required: true
+    },
 
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
 
-  password: {
-    type: String,
-    required: true
-  }
+    password: {
+        type: String,
+        required: true
+    },
+
+    role: {
+        type: String,
+        enum: ['client', 'artisan', 'admin'],
+        default: 'client'
+    }
 
 }, {
-  timestamps: true
+    timestamps: true
 })
 
 module.exports = mongoose.model('User', userSchema)
