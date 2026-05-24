94fd041f3c38dd20736445ef0b5f619cfd97545a
Ovaispecial <75292449+Ovaispecial@users.noreply.github.com>
2026-05-24 21:45:32 +0100

    Fix auth routes connection

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 require('dotenv').config()
+
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+
 const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use('/api/auth', require('./routes/authRoutes'))
-app.get('/', (req, res) => {
-  res.send('FIXORA Backend Running Successfully')
-})
+
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB Connected'))
 .catch((err) => console.log(err))
-const PORT = process.env.PORT || 5000
+
+const authRoutes = require('./routes/authRoutes')
+
+app.use('/api/auth', authRoutes)
+
+app.get('/', (req, res) => {
+    res.send('FIXORA Backend Running Successfully')
+})
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
