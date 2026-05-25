32deff7336b11273278485e97c5a4d51f8d5a62d
Ovaispecial <75292449+Ovaispecial@users.noreply.github.com>
2026-05-25 01:47:35 +0100

    Create Job model

---
+++ models/Job.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+const jobSchema = new mongoose.Schema({
+
+    customer: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    description: {
+        type: String,
+        required: true
+    },
+
+    category: {
+        type: String
+    },
+
+    budget: {
+        type: String
+    },
+
+    location: {
+        type: String
+    },
+
+    status: {
+        type: String,
+        enum: ['open', 'in-progress', 'completed'],
+        default: 'open'
+    }
+
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Job', jobSchema)
