81f285c0c4e7d3548d95c606e1e18f1a2966bdae
Ovaispecial <75292449+Ovaispecial@users.noreply.github.com>
2026-05-25 12:59:27 +0100

    Create portfolio routes

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use('/api/analytics', require('./routes/analyticsRoutes'))
 app.use('/api/availability', require('./routes/availabilityRoutes'))
 app.use('/api/profile', require('./routes/profileRoutes'))
 app.use('/api/search', require('./routes/searchRoutes'))
+app.use('/api/portfolios', require('./routes/portfolioRoutes'))
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
