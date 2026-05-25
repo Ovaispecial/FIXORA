9eef2721742cf8e74c859e97fd78570a41a5fcfa
Ovaispecial <75292449+Ovaispecial@users.noreply.github.com>
2026-05-25 07:20:32 +0100

    Create bidding system routes

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.get('/test', (req, res) => {
 app.use('/api/auth', require('./routes/authRoutes'))
 app.use('/api/artisans', require('./routes/artisanRoutes'))
 app.use('/api/jobs', require('./routes/jobRoutes'))
+app.use('/api/bids', require('./routes/bidRoutes'))
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
